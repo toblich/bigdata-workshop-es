6a763a0b2a6b98bbc353561c0d1b5cc160d5a3d5
toblich <toblich@users.noreply.github.com>
2023-02-25 20:42:53 -0300

    Landing ETL DAG

---
+++ airflow/dags/landing.py
@@ -0,0 +1,82 @@
+"""SMS ETL dag."""
+import json
+from datetime import datetime, timedelta
+from io import BytesIO
+
+import requests
+from airflow.models import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.exceptions import AirflowSkipException
+
+from minio import Minio
+from minio.error import S3Error
+
+BUCKET="sms"
+
+
+def make_minio_client():
+    return Minio(
+        "minio:9000",
+        access_key="itba-ecd",
+        secret_key="seminario",
+        secure=False,
+    )
+
+
+def create_bucket():
+    client = make_minio_client()
+
+    found = client.bucket_exists(BUCKET)
+    if not found:
+        print(f"Bucket '{BUCKET}' did not exist. Creating it...")
+        client.make_bucket(BUCKET)
+    else:
+        print(f"Bucket '{BUCKET}' already exists")
+
+
+def get_sms():
+    r = requests.get("http://sms-api:3000")
+    response = json.loads(r.content)
+    offset = response['offset']
+    data = json.dumps(response['data'])
+    print(f'Fetched SMS page with offset {offset} and size {len(response["data"])}')
+    return (offset, data)
+
+
+def land_sms(**context):
+    task_instance = context['ti']
+    (offset, data) = task_instance.xcom_pull(task_ids='get_sms_page')
+
+    if not data or (data == '[]'):
+        raise AirflowSkipException
+
+    object_name = f"sms-{offset:09d}.json"
+    print(f'Writing object {object_name} into bucket {BUCKET}')
+
+    client = make_minio_client()
+    client.put_object(
+        bucket_name=BUCKET,
+        object_name=object_name,
+        data=BytesIO(bytes(data, 'utf-8')),
+        length=len(data),
+        content_type="application/json",
+    )
+
+
+default_args = {'owner': 'tobi', 'retries': 0, 'start_date': datetime(2023, 2, 24)}
+with DAG('sms_etl', default_args=default_args, schedule_interval=timedelta(seconds=5)) as dag:
+    create_bucket_if_not_exists = PythonOperator(
+        task_id='create_bucket_if_not_exists',
+        python_callable=create_bucket
+    )
+    get_sms_page = PythonOperator(
+        task_id='get_sms_page',
+        python_callable=get_sms
+    )
+    land_sms_page = PythonOperator(
+        task_id='land_sms_page',
+        python_callable=land_sms,
+        provide_context=True
+    )
+
+    create_bucket_if_not_exists >> get_sms_page >> land_sms_page
--- api/app.js
+++ api/app.js
@@ -2,13 +2,23 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 
-const filepath = path.join(__dirname, 'SMSSpamCollection');
-const pageSize = 100;
+const filepath = path.join(__dirname, 'SMSSpamCollection'); // Downloaded from https://archive.ics.uci.edu/ml/datasets/sms+spam+collection
+const pageSize = 500;
 const dataset = fs.readFileSync(filepath, 'utf-8').split('\n');
 
 app = express();
 
-app.get('*', (req, res) => res.json(smsPage()));
+app.post('/reset', (req, res) => {
+  console.log('Resetting offset');
+  offset = 0;
+  res.json({ offset });
+});
+
+app.get('/', (req, res) => {
+  const response = smsPage();
+  res.json(response);
+  console.log(`${req.method} ${req.originalUrl} --> ${JSON.stringify(response)}`);
+});
 
 app.listen(3000, () => console.log('Listening on port 3000...'));
 
@@ -19,7 +29,7 @@ let offset = 0;
 function smsPage() {
   const page = dataset.slice(offset, offset + pageSize);
 
-  const data = page.filter(s => s).map(asJson);
+  const data = page.filter((s) => s).map(asJson);
   const response = { offset, data };
 
   offset += data.length; //* Update global offset
